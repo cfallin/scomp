3972df44bc4923bc8a9ede0b86562ab7b1c2d23e
Chris Fallin <cfallin@ece.cmu.edu>
2011-12-22 10:17:45 -0800

    barebones parser

--- parser.py
+++ parser.py
@@ -1,122 +1,53 @@
-from pyparsing import *
+def parse(s, configname, ctx):
 
-class ASTConst:
-    def __init__(self, value):
-        self.value = value
-    def evaluate(self, configname, ctx):
-        print 'eval constant: %f' % self.value
-        return self.value
-    def depends(self, configname):
-        return []
-    def __str__(self):
-        return 'ASTConst(%f)' % self.value
+    # tokenize
+    tokens = []
+    in_name = False
+    name = ''
+    for c in s:
+        if in_name:
+            if c.isalnum() or c == '.' or c == '!' or c == '_':
+                name += c
+            else:
+                tokens.append((True, name))
+                name = ''
+                in_name = False
+        if not in_name:
+            if c.isalpha() or c == '!' or c == '_':
+                in_name = True
+                name = c
+            else:
+                tokens.append((False, c))
 
-class ASTIdent:
-    def __init__(self, configname, colname):
-        self.configname = configname
-        self.colname = colname
-    def evaluate(self, configname, ctx):
-        c = self.configname
-        if c == '': c = configname
-        fullname = c + '.' + self.colname
-        print 'eval ident: %s' % fullname
-        if ctx.has_key(fullname):
-            print 'has value: %f' % ctx[fullname]
-            return ctx[fullname]
+    if name != '':
+        tokens.append((True, name))
+
+    print 'tokenized: ', tokens
+
+    # substitute
+    out = []
+    for pair in tokens:
+        if pair[0]:
+            name = pair[1]
+            print 'name', name
+            if not '.' in name:
+                name = configname + '.' + name
+            print 'resolved to', name
+            if not ctx.has_key(name):
+                raise "Unknown variable " + name
+            else:
+                print 'with value', ctx[name]
+                out.append(str(ctx[name]))
         else:
-            print 'has no value'
-            return None
-    def depends(self, configname):
-        c = self.configname
-        if c == '': c = configname
-        fullname = c + '.' + self.colname
-        return [fullname]
-    def __str__(self):
-        return 'ASTIdent(%s.%s)' % (self.configname, self.colname)
+            out.append(pair[1])
 
-class ASTOp:
-    def __init__(self, operator, val1, val2):
-        self.operator = operator
-        self.val1 = val1
-        self.val2 = val2
-    def evaluate(self, configname, ctx):
-        val1 = None
-        val2 = None
-        if self.val1 != None: val1 = self.val1.evaluate(configname, ctx)
-        if self.val2 != None: val2 = self.val2.evaluate(configname, ctx)
+    # reconstruct original expr
+    s = ''.join(out)
 
-        print 'evaluate op %s (val1 %s val2 %s)' % (self.operator, str(val1), str(val2))
+    print 'subbed: ', s
 
-        if self.operator == '+':
-            if val1 is None or val2 is None: return None
-            return val1 + val2
-        if self.operator == '-':
-            if val1 is None or val2 is None: return None
-            return val1 - val2
-        if self.operator == '*':
-            if val1 is None or val2 is None: return None
-            return val1 * val2
-        if self.operator == '/':
-            if val1 is None or val2 is None: return None
-            if val2 == 0.0: return None
-            return val1 / val2
-        if self.operator == 'unary-':
-            if val1 is None: return None
-            return -val1
-        if self.operator == 'unary+':
-            return val1
+    return eval(s, {}, {})
 
-        return None
-    def depends(self, configname):
-        d = []
-        if self.val1 != None: d.extend(val1.depends(configname))
-        if self.val2 != None: d.extend(val2.depends(configname))
-        return d
-    def __str__(self):
-        return 'ASTOp(%s,%s,%s)' % (self.operator, str(self.val1), str(self.val2))
-
-integer = Word(nums)
-real = Combine(Word(nums) + "." + Word(nums))
-bareIdent = Word(alphas + '_', bodyChars = alphanums+'_')
-fieldIdent = Word(alphas + '_!', bodyChars = alphanums+'_')
-qualifiedIdent = bareIdent + "." + fieldIdent
-
-integer.setParseAction(lambda tok: ASTConst(float(tok[0])))
-real.setParseAction(lambda tok: ASTConst(float(tok[0])))
-fieldIdent.setParseAction(lambda tok: ASTIdent('', str(tok[0])))
-qualifiedIdent.setParseAction(lambda tok: ASTIdent(str(tok[0]), tok[2].colname))
-
-operand = Forward()
-baseOperand = real | integer | qualifiedIdent | fieldIdent
-signedOperand = oneOf('- +') + baseOperand
-operand << (signedOperand | baseOperand)
-
-signedOperand.setParseAction(lambda tok: ASTOp('unary' + str(tok[0]), tok[1], None))
-
-term = Forward()
-expr = Forward()
-parenExpr = Forward()
-
-parenExpr << ("(" + expr + ")")
-term << ( (operand | parenExpr) + Group(OneOrMore(oneOf('- +') + (operand | parenExpr))) )
-expr << ( (term + Group(OneOrMore(oneOf('* /') + term))) | term )
-
-def parseSeq(tok):
-    print 'parseSeq', tok
-    astroot = tok[0]
-    tok = tok[1:]
-    while len(tok) >= 2:
-        op, val = tok[0:2]
-        tok = tok[2:]
-        astroot = ASTOp(op, astroot, val)
-    return astroot
-
-term.setParseAction(lambda tok: parseSeq(tok))
-expr.setParseAction(lambda tok: parseSeq(tok))
-parenExpr.setParseAction(lambda tok: tok[1])
-
-ast = expr.parseString('+1--3*(4*5-1)+ra.!IPC-base+!IPC', parseAll=True).asList()[0]
+print parse('+1--3*(4*5-1)+ra.!IPC-base+!IPC+ra','ra', {'ra.!IPC': 100.0, 'ra.base': 200.0, 'ra.ra': 300})
 
 # 58 + 100 - 200 + 100
-
-print ast.evaluate('ra', {'ra.!IPC': 100.0, 'ra.base': 200.0})
