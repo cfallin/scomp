6200829c4855ff313767f653dd9a689adb834791
Chris Fallin <cfallin@ece.cmu.edu>
2011-12-30 19:47:14 -0800

    update

--- scomp.py
+++ scomp.py
@@ -6,18 +6,33 @@ import simplejson as json
 
 def usage():
     sys.stderr.write("""
-    Usage: scomp model1.scomp data_dir/ sheet_dir/ model2.scomp ...
+    Usage: scomp model1.scomp [data_dir/ sheet_dir/] model2.scomp ...
+
+Each scompfile can be specified with a data and sheet directory, or
+these can be omitted (and default to 'data/' and 'sheets/' respectively).
+If more than one scompfile is specified, data and sheet directories must
+be given explicitly.
 
 """)
 
 def main(args):
-    if len(args) == 0:
+    if len(args) < 1:
         usage()
         sys.exit(1)
 
-    while len(args) >= 3:
-        fname, datadir, sheetdir = args[0:3]
-        args = args[3:]
+    shortmode = False
+    if len(args) == 1: shortmode = True
+    num = 1 if shortmode else 3
+
+    while len(args) >= num:
+        if shortmode:
+            fname = args[0]
+            args = args[1:]
+            datadir = 'data/'
+            sheetdir = 'sheets/'
+        else:
+            fname, datadir, sheetdir = args[0:3]
+            args = args[3:]
 
         m = model.model()
 
